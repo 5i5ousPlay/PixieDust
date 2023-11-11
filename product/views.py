8beda3b7f5ae85128a487c0ac2dc39583ba9ddc5
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-11 18:21:25 +0800

    added product create and list views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Product
+from .serializers import ProductCreateSerializer, ProductListSerializer
+from rest_framework import generics
 # Create your views here.
+
+
+class RegisterProduct(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductCreateSerializer
+
+
+class ProductSubclassMixin(object):
+    def get_queryset(self):
+        return Product.objects.select_subclasses()
+
+
+class ProductListView(ProductSubclassMixin, generics.ListAPIView):
+    serializer_class = ProductListSerializer
