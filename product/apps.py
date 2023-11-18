414f1b97a502acca9b18308cb47fd259b1c67f6d
cheskahung <cheska.hung@obf.ateneo.edu>
2023-11-18 21:53:14 +0800

    added Order, OrderItem, Sales Agent, and Customer to models

--- product/apps.py
+++ product/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
+
+
+class OrderItemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'order_item'
+
