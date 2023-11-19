8dc35e3240636824ffc3f56c696d53c990f8f191
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-19 11:15:10 +0800

    added choices to color field

--- inventory/models.py
+++ inventory/models.py
@@ -15,5 +15,10 @@ class Inventory(models.Model):
         ('BLACK', 'Black')
     ]
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='inventory')
-    color = models.CharField(max_length=25)
+    color = models.CharField(max_length=25, choices=COLOR_CHOICES)
     stock = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'color'], name='unique_product_color')
+        ]
