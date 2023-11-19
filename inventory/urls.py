2d9df74fe2e788bd1365e98d0484d18ba90a0a32
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-19 11:19:05 +0800

    added inventory list view

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InventoryCreateView
+from .views import InventoryCreateView, InventoryListView
 
 urlpatterns = [
     path('create/', InventoryCreateView.as_view(), name='inventory-create'),
+    path('list/', InventoryListView.as_view(), name='inventory-list'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -9,3 +9,7 @@ class InventoryCreateView(generics.CreateAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
 
+
+class InventoryListView(generics.ListAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
