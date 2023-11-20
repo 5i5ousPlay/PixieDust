b39e215e0def2fed12810feabcf2e0a50d1f0ce7
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-21 01:54:40 +0800

    initial transaction commit

---
+++ transaction/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TransactionSerializer
+from .models import Transaction
+# Create your views here.
+
+
+class TransactionListView(generics.ListAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
