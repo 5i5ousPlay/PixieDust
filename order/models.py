50a0ed101ba55544f3a90bedc43ee19189b8feda
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-21 15:03:38 +0800

    added color and personalization field to orderitem

--- order/models.py
+++ order/models.py
@@ -13,8 +13,20 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
+    COLOR_CHOICES = [
+        ('RED', 'Red'),
+        ('ORANGE', 'Orange'),
+        ('YELLOW', 'Yellow'),
+        ('GREEN', 'Green'),
+        ('BLUE', 'Blue'),
+        ('PURPLE', 'Purple'),
+        ('PINK', 'Pink'),
+        ('BLACK', 'Black')
+    ]
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orderitem')
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitem', null=True)
     discount = models.FloatField(default=0)
     quantity = models.IntegerField(default=0)
     total = models.FloatField(default=0)
+    color = models.CharField(max_length=25, choices=COLOR_CHOICES)
+    personalization = models.CharField(max_length=255, blank=True)
