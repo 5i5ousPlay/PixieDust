5962b981392fa2a294addb5f479d83c81500b77d
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-21 13:57:32 +0800

    display related fields in transaction serializer

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Transaction
 
+
 class TransactionSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+    total = serializers.SerializerMethodField()
     class Meta:
         model = Transaction
-        fields = '__all__'
+        fields = ['id', 'customer', 'agent', 'order', 'date', 'total']
         read_only_fields = ['order']
+
+    def get_date(self, obj):
+        if obj.order:
+            return obj.order.date
+        else:
+            return None
+
+    def get_total(self, obj):
+        if obj.order:
+            return obj.order.amount_due
+        else:
+            return None
