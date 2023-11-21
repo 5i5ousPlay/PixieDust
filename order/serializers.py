0a31596cc0d635176a23945a4b7ad0b8f62f83a7
RJ Conanan <rauljarodconanan@gmail.com>
2023-11-21 15:03:56 +0800

    modify orderitem and order serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,21 +1,49 @@
 from rest_framework import serializers
 from .models import OrderItem, Order
+from product.models import Product
 from transaction.models import Transaction
 from transaction.serializers import TransactionSerializer
 from django.db.models import Sum
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    personalization = serializers.CharField(max_length=255, allow_blank=True)
     class Meta:
         model = OrderItem
-        fields = ['id', 'product', 'order', 'discount', 'quantity', 'total']
+        fields = ['id', 'product', 'order', 'discount', 'quantity', 'total', 'color', 'personalization']
         read_only_fields = ['total', 'order']
 
+    def validate_personalization(self, value):
+        product_id = self.initial_data['product']
+        product = Product.objects.get(id=product_id)
+        product_personalization_limit = product.personalization
+        if len(value) > product_personalization_limit:
+            raise serializers.ValidationError("Personalization exceeds length limit")
+        return value
 
-class OrderSerializer(serializers.ModelSerializer):
+
+class OrderListSerializer(serializers.ModelSerializer):
+    customer = serializers.SerializerMethodField()
+    agent = serializers.SerializerMethodField()
+    orderitem = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['id', 'date', 'delivery_address', 'schedule', 'amount_due',
+                  'gift', 'recepient', 'customer', 'agent', 'orderitem']
+
+    def get_customer(self, obj):
+        print(obj.transaction)
+        if obj.transaction:
+            return obj.transaction.customer.id
+        else:
+            return None
+
+    def get_agent(self, obj):
+        if obj.transaction:
+            return obj.transaction.agent.id
+        else:
+            return None
 
 
 class OrderCreateSerializer(serializers.ModelSerializer):
